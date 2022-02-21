70e7dfa05335a54ed130da83a078d06fc4286f23
khalil <khalillakhdharatc@gmail.com>
2022-02-21 15:23:21 +0100

    for

---
+++ src/main/java/com/mycompany/demonetbeans/ForExemple.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demonetbeans;
+
+/**
+ *
+ * @author khali
+ */
+public class ForExemple {
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+int sum = 0;
+for(int j = 1; j<=10; j++) {
+sum = sum + j;
+System.out.println("j= "+j);
+}
+System.out.println("The sum of first 10 natural numbers is " + sum);
+}
+
+}
